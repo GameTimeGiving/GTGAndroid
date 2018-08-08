478712db3271e8ab324bbc855b54265a09a55e18
Detrick DeBurr <detrick@digitalflow.com>
2018-08-08 14:50:27 -0500

    Update CustomizeDialog.java

--- app/src/main/java/com/gametimegiving/android/CustomizeDialog.java
+++ app/src/main/java/com/gametimegiving/android/CustomizeDialog.java
@@ -1,17 +1,13 @@
 
 package com.gametimegiving.android;
 
-
 import android.app.Dialog;
 import android.content.Context;
 import android.view.Window;
 import android.view.WindowManager;
 
-
 class CustomizeDialog extends Dialog {
-
-    private Context context;
-
+   private Context context;
     CustomizeDialog(Context context) {
         super(context, R.style.Theme_Dialog_Translucent);
         try {
